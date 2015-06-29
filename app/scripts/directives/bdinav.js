689429e81dac72c7332e1d700a044253224875bf
Armand BITEAU <armandbiteau@MacBook-Pro-de-Armand.local>
2015-06-29 18:20:38 +0200

    create directive for dynamic navigation

---
+++ app/scripts/directives/bdinav.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name bdi2015App.directive:bdiNav
+ * @description
+ * # bdiNav
+ */
+angular.module('bdi2015App')
+  .directive('bdiNav', function () {
+    return {
+      templateUrl: 'views/nav.html'
+    };
+  });
